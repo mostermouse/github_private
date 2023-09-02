fc71b959652097b952d081aa3c6a17cee7cbe61d
mostermouse <whdls7946@gmail.com>
2023-09-02 14:01:44 +0900

    정처기 자바 문제

--- EPI/src/EPI/ex03.java
+++ EPI/src/EPI/ex03.java
@@ -1,9 +1,18 @@
 package EPI;
 
 public class ex03 {
-
+	private int a;
+	public void set(int a) {this.a=a;}
+	public void add(int d) {a+=d;}
+	public void print() {System.out.println(a);}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ex03 p = new ex03();
+		ex03 q;
+		p.set(10);
+		q=p;
+		p.add(10);
+		q.set(30);
+		p.print();
 
 	}
 
